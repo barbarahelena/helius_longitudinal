81f2c96fa2a1b5d793a82bcf2197d058d88e0538
Barbara Verhaar <bjh.verhaar@gmail.com>
2024-06-09 10:00:08 +0200

    xgboost scripts init

---
+++ scripts/cmb_predictions.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH -c 96
+#SBATCH --mem=68G
+#SBATCH --time=5:00:00
+#SBATCH -p 'genoa'
+eval "$(conda shell.bash hook)"
+conda activate xgb
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name diabetes \
+#     -path /projects/0/prjs0784/helius_longitudinal/diabetes \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json
+python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+    -name hypertension \
+    -path /projects/0/prjs0784/helius_longitudinal/hypertension \
+    -x class \
+    -n 200 \
+    -rand_seed 4321 \
+    -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json 
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name metsyn \
+#     -path /projects/0/prjs0784/helius_longitudinal/metsyn \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json 
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name lld \
+#     -path /projects/0/prjs0784/helius_longitudinal/lld \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json
+    
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name diabetes \
+#     -path /projects/0/prjs0784/helius_longitudinal/diabetes \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json \
+#     -permute
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name hypertension \
+#     -path /projects/0/prjs0784/helius_longitudinal/hypertension \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json \
+#     -permute
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name metsyn \
+#     -path /projects/0/prjs0784/helius_longitudinal/metsyn \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json \
+#     -permute
+# python /projects/0/prjs0784/helius_longitudinal/scripts/XGBeast.py \
+#     -name lld \
+#     -path /projects/0/prjs0784/helius_longitudinal/lld \
+#     -x class \
+#     -n 200 \
+#     -rand_seed 4321 \
+#     -param /projects/0/prjs0784/helius_longitudinal/scripts/param_grid_mb.json \
+#     -permute
