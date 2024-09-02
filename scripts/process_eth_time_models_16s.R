f5d4f238834c43f61570234932cdd77308c76a70
Barbara Verhaar <bjh.verhaar@gmail.com>
2024-09-02 23:11:53 +0200

    init

---
+++ scripts/process_eth_time_models_16s.R
@@ -0,0 +1,86 @@
+## Process results machine learning with 16s data
+
+library(tidyverse)
+library(ggpubr)
+library(ggsci)
+library(stringr)
+
+options(scipen=999)
+
+source("scripts/functions.R")
+
+## Plot assembled figure composition timepoints
+path_true <- 'timepoint_16s/output_XGB_class_timepoints_16s_2024_08_19__16-27-25'
+data_path <- 'timepoint_16s/input_data'
+labels <- c("Follow-up", "Baseline")
+
+pl2 <- plot_feature_importance_class(path_true, 20)
+grConvert::convertPicture(file.path(path_true,"Plot_AUC.pdf"), file.path(path_true,"auc.svg"))
+svg_grob <- svgparser::read_svg(file.path(path_true,"auc.svg"))
+pl3 <- plot_features_tests_top(data_path, path_true, top_n=5, nrow = 1, labels)
+plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
+                    nrow = 3, labels = c("A", "B", "C"), 
+                    heights = c(1.2,1.3,0.8))
+ggsave(plarr1, filename = "results/ml_figures/16s_comp_timepoint.pdf",
+       width = 14, height = 18)
+
+## Plot assembled figure composition timepoints - Dutch
+path_true <- 'timepoint_dutch_16s/output_XGB_class_dutch_timepoints_16s_2024_08_19__17-52-06'
+data_path <- 'timepoint_dutch_16s/input_data'
+labels <- c("Follow-up", "Baseline")
+
+pl2 <- plot_feature_importance_class(path_true, 20)
+grConvert::convertPicture(file.path(path_true,"Plot_AUC.pdf"), file.path(path_true,"auc.svg"))
+svg_grob <- svgparser::read_svg(file.path(path_true,"auc.svg"))
+pl3 <- plot_features_tests_top(data_path, path_true, top_n=5, nrow = 1, labels)
+plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
+                    nrow = 3, labels = c("A", "B", "C"), 
+                    heights = c(1.2,1.3,0.8))
+ggsave(plarr1, filename = "results/ml_figures/16s_comp_timepoint_dutch.pdf",
+       width = 14, height = 18)
+
+## Plot assembled figure composition timepoints - SAS
+path_true <- 'timepoint_sas_16s/output_XGB_class_sas_timepoints_16s_2024_08_19__19-14-17'
+data_path <- 'timepoint_sas_16s/input_data'
+labels <- c("Follow-up", "Baseline")
+
+pl2 <- plot_feature_importance_class(path_true, 20)
+grConvert::convertPicture(file.path(path_true,"Plot_AUC.pdf"), file.path(path_true,"auc.svg"))
+svg_grob <- svgparser::read_svg(file.path(path_true,"auc.svg"))
+pl3 <- plot_features_tests_top(data_path, path_true, top_n=5, nrow = 1, labels)
+plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
+                    nrow = 3, labels = c("A", "B", "C"), 
+                    heights = c(1.2,1.3,0.8))
+ggsave(plarr1, filename = "results/ml_figures/16s_comp_timepoint_sas.pdf",
+       width = 14, height = 18)
+
+## Plot assembled figure composition ethnicity - baseline
+path_true <- 'eth_base_16s/output_XGB_class_eth_baseline_16s_2024_08_19__20-32-34'
+data_path <- 'eth_base_16s/input_data'
+labels <- c("SAS", "Dutch")
+
+pl2 <- plot_feature_importance_class(path_true, 20)
+grConvert::convertPicture(file.path(path_true,"Plot_AUC.pdf"), file.path(path_true,"auc.svg"))
+svg_grob <- svgparser::read_svg(file.path(path_true,"auc.svg"))
+pl3 <- plot_features_tests_top(data_path, path_true, top_n=5, nrow = 1, labels)
+plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
+                    nrow = 3, labels = c("A", "B", "C"), 
+                    heights = c(1.2,1.3,0.8))
+ggsave(plarr1, filename = "results/ml_figures/16s_ethnicity_baseline.pdf",
+       width = 14, height = 18)
+
+## Plot assembled figure composition ethnicity - baseline
+path_true <- 'eth_fu_16s/output_XGB_class_eth_followup_16s_2024_08_19__21-53-15'
+data_path <- 'eth_fu_16s/input_data'
+labels <- c("SAS", "Dutch")
+
+pl2 <- plot_feature_importance_class(path_true, 20)
+grConvert::convertPicture(file.path(path_true,"Plot_AUC.pdf"), file.path(path_true,"auc.svg"))
+svg_grob <- svgparser::read_svg(file.path(path_true,"auc.svg"))
+pl3 <- plot_features_tests_top(data_path, path_true, top_n=5, nrow = 1, labels)
+plarr1 <- ggarrange(ggarrange(svg_grob), pl2, pl3,
+                    nrow = 3, labels = c("A", "B", "C"), 
+                    heights = c(1.2,1.3,0.8))
+ggsave(plarr1, filename = "results/ml_figures/16s_ethnicity_followup.pdf",
+       width = 14, height = 18)
+
