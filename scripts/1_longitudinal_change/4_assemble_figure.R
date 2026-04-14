2e893a149b18aceea6525a43d30f0793d0c921b7
Barbara Verhaar <bjh.verhaar@gmail.com>
2026-04-14 21:39:18 +0200

    fix labelling panels

--- scripts/1_longitudinal_change/4_assemble_figure.R
+++ scripts/1_longitudinal_change/4_assemble_figure.R
@@ -16,11 +16,10 @@ source("scripts/1_longitudinal_change/3_alphadiversity.R")
 source("scripts/1_longitudinal_change/2c_ordination.R")
 
 ## Assemble Figure 1
-fig1 <- ggarrange(ggarrange(pl_fig1_A, pl_fig1_B, widths = c(1.3, 1)),
-                  ggarrange(pl_fig1_C, pl_fig1_D, pl_fig1_E, nrow = 1),
-                  ggarrange(pl_fig1_F, pl_fig1_G, widths = c(1.25, 1)),
-                  nrow = 3, heights = c(1, 1, 1.25),
-                  labels = c("A", "B", "C", "D", "E", "F", "G"))
+fig1 <- ggarrange(ggarrange(pl_fig1_A, pl_fig1_B, widths = c(1.3, 1), labels = c("A", "B")),
+                  ggarrange(pl_fig1_C, pl_fig1_D, pl_fig1_E, nrow = 1, labels = c("C", "D", "E")),
+                  ggarrange(pl_fig1_F, pl_fig1_G, widths = c(1.25, 1), labels = c("F", "G")),
+                  nrow = 3, heights = c(1, 1, 1.25))
 
 ggsave(fig1, filename = "results/1_longitudinal_change/figure1.pdf", 
           width = 13, height = 15, device = "pdf")
